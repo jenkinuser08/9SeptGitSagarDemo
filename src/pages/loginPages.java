6e5875ed00d4a5ad0403ca8856534d69792e88bc
sagar <sagar@gmail.com>
2024-02-02 16:22:33 +0530

    login pages

--- src/pages/loginPages.java
+++ src/pages/loginPages.java
@@ -4,9 +4,8 @@ public class loginPages {
 	
 	public void loginToApp()
 	{
-
 		System.out.println("login   ToApp");
-                 
-   }
+                 System.out.println("Entering user name of user2");
+	}
 
 }
